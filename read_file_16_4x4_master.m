d2b0d1b44b2cf68f1fd2b91316368aead15475ca
admin <admin@US2016-2>
2021-05-20 14:15:30 +0900

    created

--- read_file_16_4x4_master.m
+++ read_file_16_4x4_master.m
@@ -17,7 +17,7 @@ pos_frame=strfind(path,'frame');
 pos_freq=strfind(path,'kHz');
 path_name1=path(pos_tof+9:pos_frame-2);
 path_name2=path(pos_frame:end-1);
-Fc=str2num(path(pos_freq-2:pos_freq-1));% [kHz]
+Fc=str2double(path(pos_freq-2:pos_freq-1));% [kHz]
 Fc=1000*Fc;% [Hz]
 
 ch_str = num2str(1,'%02d') ;
@@ -29,6 +29,7 @@ xtime = (0:dnum-1)*(1/Fs)*1000;%[mx]
 
 xmin=0; xmax=15;% 0-15ms
 ymin=0; ymax=1.1;
+xs=zeros(1,16);
 
 f1=figure(1);
 set(f1,'Position', [700 500 1200 800])
@@ -39,14 +40,35 @@ set(f2,'Position', [600 300 1200 800])
 f3=figure(3);
 set(f3,'Position', [500 100 1200 800])
 
+%% Filter 設定
+iftr=1;
 
-
+if iftr==1
+    Fstop = 7000;
+    Fpass = 8000;
+    Astop = 30;
+    Apass = 0.5;
+    
+    d = designfilt('highpassfir','StopbandFrequency',Fstop, ...
+        'PassbandFrequency',Fpass,'StopbandAttenuation',Astop, ...
+        'PassbandRipple',Apass,'SampleRate',Fs,'DesignMethod','equiripple');
+    
+    delay = round(mean(grpdelay(d)))+1;
+end
 
 %%
 for ix=1:16
     ch_str = num2str(ix,'%02d');
     if ix ~= 1
         file(3:4) = ch_str ;
+    end
+    
+    % Filter 処理
+    if iftr==1
+        [wdata_tmp,Fs] = audioread([path file]) ;
+        wdata=filter(d,wdata_tmp) ;
+        wdata(1:end-delay+1) = wdata(delay:end);
+    else
         [wdata,Fs] = audioread([path file]);
     end
     
@@ -54,9 +76,21 @@ for ix=1:16
     wdm = complex(a, b);
     
     
+    % xrange
+    wdm_abs=abs(wdm);
+    for i=1:dnum
+        if wdm_abs(i+15,1)-wdm_abs(i,1)>0.04
+            x1=xtime(1,i);
+            break
+        end
+    end
+    
+    xs(1,ix)=x1;
+    
+    
     figure(1);subplot(4,4,ix)
     pl = plot(xtime,wdata); set(pl,ps)
-    xlim([xmin,xmax]);ylim([-ymax ymax]);
+    xlim([x1,x1+1.2]);ylim([-ymax ymax]);
     ylabel('Amplitude')
     
     if ix==2
@@ -73,7 +107,7 @@ for ix=1:16
 
     figure(2);subplot(4,4,ix)
     pl = plot(xtime,abs(wdm)); set(pl,ps)
-    xlim([xmin xmax]);ylim([0 ymax]);
+    xlim([x1,x1+1.2]);ylim([0 1.2]);
     ylabel('Amplitude')
     if ix==2
         tp=title(path_name1);set(tp,tx)
@@ -87,7 +121,7 @@ for ix=1:16
     
     figure(3);subplot(4,4,ix)
     pl = plot(xtime,angle(wdm)); set(pl,ps)
-    xlim([xmin xmax]);ylim([-3.99 3.99]);
+    xlim([x1,x1+1.2]);ylim([-3.99 3.99]);
     ylabel('Phase[rad]')
     if ix==2
         tp=title(path_name1);set(tp,tx)
@@ -99,5 +133,5 @@ for ix=1:16
     text(10,3.5,['CH : ' ch_str],'FontSize',12)
     set(gca,ax)
     
-    
+
 end
