af01af63ff2f6e45356612381a381e8bd050e547
admin <admin@US2016-2>
2021-06-30 19:51:01 +0900

    位相の表示

---
+++ phase.m
@@ -0,0 +1,140 @@
+%
+% wave ファイルのプロット
+%
+%%
+clearvars
+close all
+
+% 図の基本設定
+ax.LineWidth=1.2;ax.FontSize=12;
+ps.Color='black';ps.LineWidth=1.5;
+tx.Interpreter='none';
+% スプレッドシート
+filename1 = '6_sonar_tr4x4_pos_b41.csv';
+T1 = readtable(filename1);
+S1 = table2struct(T1);
+filename2 = '1_meas_item_list_rev.xlsx';
+T2 = readtable(filename2);
+S2 = table2struct(T2);
+%%
+[file,path] = uigetfile('.\2_tof_data\*.wav','Select a File');
+pos_tof=strfind(path,'tof_data');
+pos_frame=strfind(path,'frame');
+pos_freq=strfind(path,'kHz');
+path_name1=path(pos_tof+9:pos_frame-2);
+path_name2=path(pos_frame:end-1);
+Fc=str2double(path(pos_freq-2:pos_freq-1));% [kHz]
+Fc=1000*Fc;% [Hz]
+
+%% 実験No
+No=str2double(path_name1(1:2));
+
+%% マイクの位置
+pos=str2double(path_name2(8:9));
+pos=rem(pos,16);
+if pos==0
+    pos=16;
+end
+
+oppos=str2double(file(3:4));
+
+ch_str = num2str(1,'%02d') ;
+file(3:4) = ch_str ;
+[wdata,Fs] = audioread([path file]) ;
+
+dnum=length(wdata);
+xtime = (0:dnum-1)*(1/Fs)*1000;%[mx]
+
+xmin=0; xmax=15;% 0-15ms
+ymin=0; ymax=1.1;
+
+%% 温度
+if No<9
+    No=1;
+elseif 8 < No&&No <17
+    No=9;
+elseif 16 < No&&No <25
+    No=17;
+else
+    No=25;
+end
+T=tem((S2(No).x____testo____________+S2(No).Var11)/2);
+
+
+f1=figure(1);
+set(f1,'Position', [500 100 1200 800])
+
+f2=figure(2);
+set(f2,'Position', [600 100 1200 800])
+
+
+%% Filter 設定
+iftr=1;
+
+if iftr==1
+    Fstop = 7000;
+    Fpass = 8000;
+    Astop = 30;
+    Apass = 0.5;
+    
+    d = designfilt('highpassfir','StopbandFrequency',Fstop, ...
+        'PassbandFrequency',Fpass,'StopbandAttenuation',Astop, ...
+        'PassbandRipple',Apass,'SampleRate',Fs,'DesignMethod','equiripple');
+    
+    delay = round(mean(grpdelay(d)))+1;
+end
+
+%%
+
+% 時間窓設定
+idxmbase=fix((leng(S1(pos).xpos,S1(pos).ypos,S1(oppos).xpos,S1(oppos).ypos)*192000/T+0.18*192));
+if idxmbase < 0
+    idxmbase = 1;
+end
+time.s=xtime(idxmbase)-0.18;
+time.e=xtime(idxmbase+192)-0.18;
+
+wdmpoint = idxmbase+96;
+
+% Filter 処理
+if iftr==1
+    [wdata_tmp,Fs] = audioread([path file]) ;
+    wdata=filter(d,wdata_tmp) ;
+    wdata(1:end-delay+1) = wdata(delay:end);
+else
+    [wdata,Fs] = audioread([path file]);
+end
+
+[b, a] = demod(wdata, Fc, Fs, 'qam');
+wdm = complex(a, b);
+
+    
+figure(1);
+pl = plot(xtime,angle(wdm)); set(pl,ps)
+xlim([time.s time.e]);ylim([-3.99 3.99]);
+ylabel('Phase[rad]')
+xlabel('Time(ms)')
+text(10,3.5,['CH : ' ch_str],'FontSize',12)
+set(gca,ax)
+    
+figure(2);
+hold on
+for i=0:9
+    pl =plot(wdm(wdmpoint+i),'o'); set(pl,ps)
+    set(gca,ax)
+end
+xlim([-1.1 1.1]);ylim([-1.1 1.1])
+grid on
+hold off
+
+%% templeture
+function T=tem(t)
+    T=331.5+0.6*t;
+end
+
+%% length
+function len=leng(x1,y1,x2,y2)
+    l=(x1-x2)^2+(y1-y2)^2;
+    len=sqrt(l);
+end
+
